a826533fb6f3752b96ba232cbd0778fffad2655b
Efraim Munthe <efrayanglain@gmail.com>
2024-07-31 22:08:20 +0700

    set up tailwind

---
+++ resources/views/app.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel + Alpine</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+    @vite('resources/css/app.css')
+</head>
+
+<body class="font-sans antialiased dark:bg-black dark:text-white/50">
+    <h1>Laravel + Alpine demo</h1>
+    <p>Just trying out a new tech stack.</p>
+</body>
+
+</html>
