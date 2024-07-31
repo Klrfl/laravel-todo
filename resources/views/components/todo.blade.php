9e1793338ad5ab9a48547b04bb81e4b6ca6ff880
Efraim Munthe <efrayanglain@gmail.com>
2024-07-31 22:08:52 +0700

    implement deletion and mark as done feature

--- resources/views/components/todo.blade.php
+++ resources/views/components/todo.blade.php
@@ -1,4 +1,20 @@
-<li>
-    <button title="click to mark as done"
-        class="p-2 block w-full text-left bg-gray-900 dark:hover:text-neutral-200">{{ $todo->label }}</button>
+<li id="wrapper-{{ $todo->id }}">
+    <form hx-swap="outerHTML" hx-target="#wrapper-{{ $todo->id }}">
+        @csrf
+        <span class="p-2 block w-full text-left bg-gray-900 dark:hover:text-neutral-200">
+            {{ $todo->label }}
+        </span>
+
+        <button hx-put="/todo/{{ $todo->id }}" title="click to mark as done">
+            @if (!$todo->is_done)
+                mark as done
+            @else
+                mark as not done
+            @endif
+        </button>
+
+        <button hx-delete="/todo/{{ $todo->id }}">
+            Delete
+        </button>
+    </form>
 </li>
--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,32 @@ Route::post("/todo", function (Request $request) {
     return view("components.todo", ['todo' => $newTodo]);
 });
 
-Route::get("/todo/{id}", function (string $id) {
-    $todo = Todo::find($id);
+Route::put("/todo/{id}", function (Request $request, string $id) {
+    $todo = Todo::query()->findOrFail($id);
+
+    if ($request->has('label')) {
+        $todo->label = $request->label;
+    }
+
+    if ($request->has('is_done')) {
+        $todo->is_done = $request->is_done;
+    } else {
+        $todo->is_done = !$request->is_done;
+    }
+
+    $todo->saveOrFail();
+
     return view("components.todo", ["todo" => $todo]);
+});
+
+Route::delete("/todo/{id}", function (string $id) {
+    $deletedTodo = Todo::query()
+        ->where('id', $id)
+        ->delete($id);
+
+    if ($deletedTodo != 1) {
+        return response('not deleted', 500);
+    }
+
+    return response()->noContent(200);
 });
