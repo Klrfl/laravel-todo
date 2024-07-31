bea0eb4946679e5dace1d6ec0496c560b27377ea
Efraim Munthe <efrayanglain@gmail.com>
2024-07-31 22:08:42 +0700

    set up base layout and main page

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,22 +1,15 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<x-base-layout title="Laravel Todo with Alpine and HTMX">
+    <main class="max-w-7xl mx-auto">
+        <h1 class="my-2 text-lg font-bold">Todo Laravel + Alpine + HTMX</h1>
+        <p>Trying out a new tech stack.</p>
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Laravel + Alpine</title>
-
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-
-    @vite('resources/css/app.css')
-</head>
-
-<body class="font-sans antialiased dark:bg-black dark:text-white/50">
-    <h1>Laravel + Alpine demo</h1>
-    <p>Just trying out a new tech stack.</p>
-</body>
-
-</html>
+        <ul class="flex flex-col">
+            <li>SOme todo item</li>
+            <li>Some todo item</li>
+            <li>Some todo item</li>
+            <li>Some todo item</li>
+            <li>Some todo item</li>
+            <li>Some todo item</li>
+        </ul>
+    </main>
+</x-base-layout>
