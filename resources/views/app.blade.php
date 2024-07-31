5eb76e945b3b6db21cf9451de79a88c327481e7c
Efraim Munthe <efrayanglain@gmail.com>
2024-07-31 22:08:47 +0700

    spice up the ui part 2

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,6 +1,6 @@
 <x-base-layout title="Laravel Todo with Alpine and HTMX">
-    <main class="max-w-5xl mx-auto py-4 flex flex-wrap">
-        <header class="outline outline-gray-100 dark:outline-gray-900 p-4 flex-1">
+    <main class="max-w-4xl mx-auto py-4 flex flex-col gap-4 md:flex-row">
+        <header class="outline outline-gray-100 dark:outline-gray-900 p-4 md:basis-1/3">
             <h1 class="mb-2 text-4xl font-bold">Todo Laravel + Alpine + HTMX</h1>
             <p>Trying out a new tech stack.</p>
 
@@ -10,15 +10,15 @@
             </form>
         </header>
 
-        <ul class="p-4 flex flex-col flex-1">
+        <ul class="flex flex-col gap-4 md:flex-1">
             @empty($todos)
                 <li>you have got nothing to do today.</li>
             @endempty
 
             @foreach ($todos as $todo)
-                <li class="p-2 bg-gray-900">
-                    <span>{{ $todo->label }}</span>
-                    <button title="mark as done">👍</button>
+                <li>
+                    <button title="click to mark as done"
+                        class="p-2 block w-full text-left bg-gray-900 dark:hover:text-neutral-200">{{ $todo->label }}</button>
                 </li>
             @endforeach
         </ul>
